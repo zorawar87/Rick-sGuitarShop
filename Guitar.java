d7f0b1b0e5a7ad786fd38968de4c1f94cf78fc93
zorawar <zorawar87@gmail.com>
2017-09-23 18:22:35 -0400

    skel

--- Guitar.java
+++ Guitar.java
@@ -1,14 +1,47 @@
-enum GuitarType{
+enum GuitarType {
   ACOUSTIC, ELECTRIC
 }
 
-enum GuitarWood{
-  ROSEWOOD,ALDER,MAHOGANY,CEDAR
+enum GuitarWood {
+  ROSEWOOD, ALDER, MAHOGANY, CEDAR
 }
 
-public class Guitar{
-   int sno,price;
-  String brand, model;
+enum GuitarBrand {
+  GIBSON, FENDER, IBANEZ, PAUL_REED_SMITH, EPIPHONE, JACKSON, TAYLOR, MARTIN, YAMAHA, RICKENBACKER
+}
+
+public class Guitar {
+  static int COUNTER = 0x0;
+  int sno;
+  GuitarBrand brand;
+  String model;
+  int price;
   GuitarType type;
   GuitarWood topWood, backWood;
+
+  public Guitar (GuitarBrand b, String m, int p, GuitarType t, GuitarWood tw, GuitarWood bw) {
+    sno = ++COUNTER;
+    brand = b;
+    model = m;
+    price = p;
+    type = t;
+    topWood = tw;
+    backWood = bw;
+  }
+
+  public int getSno() { return sno; }
+  public GuitarBrand getBrand() { return brand; }
+  public String getModel() { return model; }
+  public int getPrice() { return price; }
+  public GuitarType getType() { return type; }
+  public GuitarWood getTopWood () { return topWood; }
+  public GuitarWood getBackWood () { return backWood; }
+
+  public void setSno ( int value ) { sno = value; }
+  public void setBrand ( GuitarBrand value ) { brand = value; }
+  public void setModel ( String value ) { model = value; }
+  public void setPrice ( int value ) { price = value; }
+  public void setType ( GuitarType value ) { type = value; }
+  public void setTopWood ( GuitarWood value ) { topWood = value; }
+  public void setBackWood ( GuitarWood value ) { backWood = value; }
 }
--- Inventory.java
+++ Inventory.java
@@ -1,5 +1,18 @@
-import java.util.Maps;
+import java.util.HashMap;
 
-public class Inventory{
-  Map<int,Guitar> collection; // collection<sn> = Guitar
+public class Inventory {
+  public HashMap<Integer, Guitar> collection, sales; //available collection, sales
+
+  public Inventory() {
+    collection = new HashMap<Integer, Guitar>();
+  }
+
+  public boolean addToCollection (Guitar g) {
+    if (g == collection.put (g.getSno(), g))
+      return true;
+    return false;
+  }
+  public boolean removeFromCollection (Guitar g) {
+    return collection.remove (g.getSno(), g);
+  }
 }
