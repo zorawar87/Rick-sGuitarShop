cf8aa8bd3f9ebda86e3a120c7b445eb39936c3d7
zorawar <zorawar87@gmail.com>
2017-09-24 10:17:24 -0400

    basic structure is ready.

--- Guitar.java
+++ Guitar.java
@@ -1,26 +1,35 @@
 enum GuitarType {
-  ACOUSTIC, ELECTRIC
+  ACOUSTIC, ELECTRIC;
 }
 
 enum GuitarWood {
-  ROSEWOOD, ALDER, MAHOGANY, CEDAR
+  ROSEWOOD, ALDER, MAHOGANY, CEDAR;
 }
 
 enum GuitarBrand {
-  GIBSON, FENDER, IBANEZ, PAUL_REED_SMITH, EPIPHONE, JACKSON, TAYLOR, MARTIN, YAMAHA, RICKENBACKER
+  GIBSON, FENDER, IBANEZ, PAUL_REED_SMITH, EPIPHONE, JACKSON, TAYLOR, MARTIN, YAMAHA, RICKENBACKER;
 }
 
 public class Guitar {
-  static int COUNTER = 0x0;
+  static int COUNTER = 0;
   int sno;
   GuitarBrand brand;
   String model;
-  int price;
+  float price;
   GuitarType type;
   GuitarWood topWood, backWood;
 
+  public Guitar () {
+    sno = -1;
+    brand = null;
+    model = null;
+    price = -1;
+    type = null;
+    topWood = null;
+    backWood = null;
+  }
   public Guitar (GuitarBrand b, String m, int p, GuitarType t, GuitarWood tw, GuitarWood bw) {
-    sno = ++COUNTER;
+    sno = -1;
     brand = b;
     model = m;
     price = p;
@@ -32,7 +41,7 @@ public class Guitar {
   public int getSno() { return sno; }
   public GuitarBrand getBrand() { return brand; }
   public String getModel() { return model; }
-  public int getPrice() { return price; }
+  public float getPrice() { return price; }
   public GuitarType getType() { return type; }
   public GuitarWood getTopWood () { return topWood; }
   public GuitarWood getBackWood () { return backWood; }
@@ -40,8 +49,12 @@ public class Guitar {
   public void setSno ( int value ) { sno = value; }
   public void setBrand ( GuitarBrand value ) { brand = value; }
   public void setModel ( String value ) { model = value; }
-  public void setPrice ( int value ) { price = value; }
+  public void setPrice ( float value ) { price = value; }
   public void setType ( GuitarType value ) { type = value; }
   public void setTopWood ( GuitarWood value ) { topWood = value; }
   public void setBackWood ( GuitarWood value ) { backWood = value; }
+
+  public String toString () {
+    return String.format ("sno: %03d\t brand: %s\t model: %s\t price: %.2f\t type: %s\t top-wood: %s\t bottom-wood: %s\t", sno, brand, model, price, type, topWood, backWood);
+  }
 }
