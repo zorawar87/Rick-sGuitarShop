5d075ba846c16d3814c06920ff7295c05c0427f3
zorawar <zorawar87@gmail.com>
2017-09-24 10:17:02 -0400

    supports basic insert/del

--- Inventory.java
+++ Inventory.java
@@ -8,10 +8,20 @@ public class Inventory {
   }
 
   public boolean addToCollection (Guitar g) {
+    g.COUNTER++;
+    g.setSno (g.COUNTER);
+    return g == collection.put (g.getSno(), g);
+  }
+
+  public boolean addToCollection (GuitarBrand b, String s, int p, GuitarType t, GuitarWood tw, GuitarWood bw) {
+    Guitar g = new Guitar (b, s, p, t, tw, bw);
+    g.COUNTER++;
+    g.setSno (g.COUNTER);
     if (g == collection.put (g.getSno(), g))
       return true;
     return false;
   }
+
   public boolean removeFromCollection (Guitar g) {
     return collection.remove (g.getSno(), g);
   }
