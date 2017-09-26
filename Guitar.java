61ad5999f008bad622d56c3a1c519ebd59c079fe
zorawar <zorawar87@gmail.com>
2017-09-26 09:33:14 -0400

    improved structure, declared mod method, and ramped up UI

--- Guitar.java
+++ Guitar.java
@@ -19,16 +19,23 @@ public class Guitar {
   GuitarType type;
   GuitarWood topWood, backWood;
 
+  /**
+   * Default Constructor
+   */
   public Guitar () {
-    sno = -1;
-    brand = null;
-    model = null;
-    price = -1;
-    type = null;
-    topWood = null;
-    backWood = null;
+    this (null, null, -1, null, null, null);
   }
-  public Guitar (GuitarBrand b, String m, int p, GuitarType t, GuitarWood tw, GuitarWood bw) {
+
+  /**
+   * Parameterized Constructor
+   * @param b brand of the guitar
+   * @param m model of the guitar
+   * @param p price of the guitar
+   * @param t type of the guitar
+   * @param tw top-wood type of the guitar
+   * @param bw back-wood type of the guitar
+   */
+  public Guitar (GuitarBrand b, String m, float p, GuitarType t, GuitarWood tw, GuitarWood bw) {
     sno = -1;
     brand = b;
     model = m;
@@ -38,23 +45,83 @@ public class Guitar {
     backWood = bw;
   }
 
+  /**
+   * Get the Serial number
+   * @return int
+   */
   public int getSno() { return sno; }
+  /**
+   * Get the guitar brand
+   * @return GuitarBrand
+   */
   public GuitarBrand getBrand() { return brand; }
+  /**
+   * Get the guitar model
+   * @return String
+   */
   public String getModel() { return model; }
+  /**
+   * Get the guitar price
+   * @return float
+   */
   public float getPrice() { return price; }
+  /**
+   * Get the guitar type
+   * @return GuitarType
+   */
   public GuitarType getType() { return type; }
+  /**
+   * Get the Guitar's top wood type
+   * @return GuitarWood
+   */
   public GuitarWood getTopWood () { return topWood; }
+  /**
+   * Get the Guitar's back wood type
+   * @return GuitarWood
+   */
   public GuitarWood getBackWood () { return backWood; }
 
-  public void setSno ( int value ) { sno = value; }
-  public void setBrand ( GuitarBrand value ) { brand = value; }
-  public void setModel ( String value ) { model = value; }
-  public void setPrice ( float value ) { price = value; }
-  public void setType ( GuitarType value ) { type = value; }
-  public void setTopWood ( GuitarWood value ) { topWood = value; }
-  public void setBackWood ( GuitarWood value ) { backWood = value; }
+  /**
+   * Set the guitar's serial number
+   * @return Guitar Object
+   */
+  public Guitar setSno ( int value ) { sno = value; return this; }
+  /**
+   * Set the guitar's brand
+   * @return Guitar Object
+   */
+  public Guitar setBrand ( GuitarBrand value ) { brand = value; return this; }
+  /**
+   * Set the guitar's model
+   * @return Guitar Object
+   */
+  public Guitar setModel ( String value ) { model = value; return this; }
+  /**
+   * Set the guitar's price
+   * @return Guitar Object
+   */
+  public Guitar setPrice ( float value ) { price = value; return this; }
+  /**
+   * Set the guitar's type
+   * @return Guitar Object
+   */
+  public Guitar setType ( GuitarType value ) { type = value; return this; }
+  /**
+   * Set the guitar's top wood type
+   * @return Guitar Object
+   */
+  public Guitar setTopWood ( GuitarWood value ) { topWood = value; return this; }
+  /**
+   * Set the guitar's back wood type
+   * @return Guitar Object
+   */
+  public Guitar setBackWood ( GuitarWood value ) { backWood = value; return this; }
 
+  /**
+   * String Representation of a Guitar Object
+   * @return formatted string representation
+   */
   public String toString () {
-    return String.format ("sno: %03d\t brand: %s\t model: %s\t price: %.2f\t type: %s\t top-wood: %s\t bottom-wood: %s\t", sno, brand, model, price, type, topWood, backWood);
+    return String.format ("\t|  %03d  | %-19s | %-18s |  %.2f   |     %-10s     | %-10s | %-10s |\n", sno, brand, model, price, type, topWood, backWood);
   }
 }
--- Inventory.java
+++ Inventory.java
@@ -3,26 +3,61 @@ import java.util.HashMap;
 public class Inventory {
   public HashMap<Integer, Guitar> collection, sales; //available collection, sales
 
+  /**
+   * Default Constructor
+   */
   public Inventory() {
     collection = new HashMap<Integer, Guitar>();
+    sales = new HashMap<Integer, Guitar>();
   }
 
+  /**
+   * Creates guitar from given parameters and hands off adding to collection to its polymorphic counterpart
+   * @param g guitar object
+   * @return true if successfully added to collection
+   */
   public boolean addToCollection (Guitar g) {
     g.COUNTER++;
     g.setSno (g.COUNTER);
     return g == collection.put (g.getSno(), g);
   }
 
-  public boolean addToCollection (GuitarBrand b, String s, int p, GuitarType t, GuitarWood tw, GuitarWood bw) {
-    Guitar g = new Guitar (b, s, p, t, tw, bw);
-    g.COUNTER++;
-    g.setSno (g.COUNTER);
-    if (g == collection.put (g.getSno(), g))
-      return true;
-    return false;
+  /**
+   * Creates guitar from given parameters and hands off adding to collection to its polymorphic counterpart
+   * @param b brand of the guitar
+   * @param m model of the guitar
+   * @param p price of the guitar
+   * @param t type of the guitar
+   * @param tw top-wood type of the guitar
+   * @param bw back-wood type of the guitar
+   * @return true if successfully added to collection
+   */
+  public boolean addToCollection (GuitarBrand b, String m, float p, GuitarType t, GuitarWood tw, GuitarWood bw) {
+    Guitar g = new Guitar (b, m, p, t, tw, bw);
+    return addToCollection (g);
   }
 
+  /**
+   * Removes given guitar from collection
+   * @param g guitar to remove
+   * @return true if removed successfully and added to sales map
+   */
   public boolean removeFromCollection (Guitar g) {
-    return collection.remove (g.getSno(), g);
+    return collection.remove (g.getSno(), g) && g == sales.put (g.getSno(), g);
   }
+
+  /**
+   * Replaces guitar if the serial no exists, else adds to collection
+   * <p>
+   * In case serial no is not found, adding to collection is handed off to the addToCollection method
+   *
+   * @param g guitar to replace/add
+   * @return true if successfully added/replaced
+   */
+  public boolean replaceGuitar (Guitar g) {
+    if (collection.containsKey (g.getSno()))
+      return g == collection.put (g.getSno(), g);
+    return addToCollection (g);
+  }
+
 }
